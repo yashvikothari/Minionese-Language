19e1f095b7af3758eb7ab6c4980f8129513fd86f
Yashvi <yashvikothari.ict18@gmail.com>
2023-01-09 18:29:56 +0530

    updateBananaSpeaks

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
-
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
-
-function getTranslationURL(input) {
-  return serverURL + "?" + "text=" + input;
+var output = document.querySelector("#output");
+function clickEventHandler() {
+  translate(txtInput.value);
 }
 
-function errorHandler(error) {
-  console.log("error occured", error);
-  alert("something wrong with server! try again after some time");
-}
 
-function clickHandler() {
-  var inputText = txtInput.value; // taking input
 
-  // calling server for processing
-  fetch(getTranslationURL(inputText))
-    .then((response) => response.json())
-    .then((json) => {
-      var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText; // output
+function translate(inputText) {
+  fetch(
+    `https://api.funtranslations.com/translate/minion.json?text=${inputText}`
+  )
+    .then(function (response) {
+      return response.json();
     })
-    .catch(errorHandler);
+    .then(function (json) {
+      output.innerHTML = json.contents.translated;
+    });
 }
 
-btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("click", clickEventHandler);
