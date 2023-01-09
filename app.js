de13b0a4f4c1083ad07f9d2eb843a09f0025b358
Yashvi Kothari <43522677+yashvikothari@users.noreply.github.com>
2023-01-09 18:36:19 +0530

    comparev2

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var output = document.querySelector("#output");
-function clickEventHandler() {
-  translate(txtInput.value);
+// function clickEventHandler() {
+//   translate(txtInput.value);
+// }
+
+
+
+// function translate(inputText) {
+//   fetch(
+//     `https://api.funtranslations.com/translate/minion.json?text=${inputText}`
+//   )
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (json) {
+//       output.innerHTML = json.contents.translated;
+//     });
+// }
+
+// btnTranslate.addEventListener("click", clickEventHandler);
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
 }
 
 
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
 
-function translate(inputText) {
-  fetch(
-    `https://api.funtranslations.com/translate/minion.json?text=${inputText}`
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (json) {
-      output.innerHTML = json.contents.translated;
-    });
-}
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
 
-btnTranslate.addEventListener("click", clickEventHandler);
+btnTranslate.addEventListener("click", clickHandler)
